aef0a926acfca3f2ef8abc7789f4e9d484e681ea
Adilson <adilsonchy@gmail.com>
2026-03-20 15:31:27 +0000

    feat: Adicionando o teste de roteamento App.test.jsx e configurando o Jest

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,10 @@
 require("@testing-library/jest-dom");
 // 膆 Silencia o warning do React sobre JSX transform antigo
 const originalWarn = console.warn;
+
+import { TextEncoder, TextDecoder } from "util";
+if (!global.TextEncoder) global.TextEncoder = TextEncoder;
+if (!global.TextDecoder) global.TextDecoder = TextDecoder; 
 console.warn = (...args) => {
  if (
  typeof args[0] === "string" &&
